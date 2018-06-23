a869e4156b38ccc84b75906e4d4a2ccc370b1595
Band-aid <da-ishi@daichibox.com>
2018-06-23 17:13:38 +0900

    fixed issue #6
    1. Fixed issue #6
    2. Cleaned up code for better memory management
     - Added Auth Header as constructor

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const QiitaAPI = require('./qiita-api')
+const Qiita = new QiitaAPI
 const path = require('path')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,62 +12,61 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    let launch = vscode.commands.registerCommand('extension.launch',function (){
-    console.log('Congratulations, your extension "go-qiita" is now active!');
+    let launch = vscode.commands.registerCommand('extension.launch', function () {
+        console.log('Congratulations, your extension "go-qiita" is now active!');
     })
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-   
+
     let upload = vscode.commands.registerCommand('extension.post', function () {
         // The code you place here will be executed every time your command is executed
-        let editor = vscode.window.activeTextEditor; 
-        if (!editor) 
+        let editor = vscode.window.activeTextEditor;
+        if (!editor)
             return; // No open text editor
-        
+
         let firstLine = editor.document.lineAt(1);
         let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-        let textrange = new vscode.Range(1,firstLine.range.start.character,editor.document.lineCount - 1,lastLine.range.end.character);
+        let textrange = new vscode.Range(1, firstLine.range.start.character, editor.document.lineCount - 1, lastLine.range.end.character);
         let body = editor.document.getText(textrange);
         let title = path.basename(editor.document.fileName);
-        let options = editor.document.getText(new vscode.Range(0,0,1,firstLine.range.end.character))
+        let options = editor.document.getText(new vscode.Range(0, 0, 1, firstLine.range.end.character))
         console.log(options)
-        if(!title.includes('.md')){
-           vscode.window.showInformationMessage('please save and name your file with a .md extension');
-          }
-        else 
-        {
-          var uploadtitle = title.replace(".md","")
-          QiitaAPI.post(body,uploadtitle,JSON.parse(options))
+        if (!title.includes('.md')) {
+            vscode.window.showInformationMessage('please save and name your file with a .md extension');
         }
-        
+        else {
+            var uploadtitle = title.replace(".md", "")
+            Qiita.post(body, uploadtitle, JSON.parse(options))
+        }
+
         //vscode.window.showInformationMessage(text+ ' ' + title);
-        
+
     })
-    
+
     let createTemplate = vscode.commands.registerCommand('extension.createTemplate', function () {
         let document = vscode.window.activeTextEditor
-        let template = {"id":"","private":true,"tags":[]}
-                if (!createTemplate) 
+        let template = { "id": "", "private": true, "tags": [] }
+        if (!createTemplate)
             return; // No open text editor
-        document.edit((builder)=>{
+        document.edit((builder) => {
             let startPos = document.document.positionAt(0)
-            builder.insert(startPos,JSON.stringify(template))
+            builder.insert(startPos, JSON.stringify(template))
         })
     })
 
-    let fix = vscode.commands.registerCommand('extension.update',function () {
+    let fix = vscode.commands.registerCommand('extension.update', function () {
         let article = vscode.window.activeTextEditor
         let firstLine = article.document.lineAt(1);
         let lastLine = article.document.lineAt(article.document.lineCount - 1);
-        let textrange = new vscode.Range(1,firstLine.range.start.character,article.document.lineCount - 1,lastLine.range.end.character);
+        let textrange = new vscode.Range(1, firstLine.range.start.character, article.document.lineCount - 1, lastLine.range.end.character);
         let body = article.document.getText(textrange);
         let title = path.basename(article.document.fileName);
-        var uploadtitle = title.replace(".md","")
+        var uploadtitle = title.replace(".md", "")
         let tagline = article.document.lineAt(1);
-        let tags = article.document.getText(new vscode.Range(0,0,1,tagline.range.end.character))
+        let tags = article.document.getText(new vscode.Range(0, 0, 1, tagline.range.end.character))
         let options = JSON.parse(tags)
-        QiitaAPI.patch(body,uploadtitle,options)
+        Qiita.patch(body, uploadtitle, options)
     })
 
     context.subscriptions.push(launch)
--- qiita-api.js
+++ qiita-api.js
@@ -5,74 +5,80 @@ const accessToken = ''
 const baseurl = 'https://qiita.com/api/v2/'
 //const QiitaRequest = require('./api-manager')
 
-class QiitaAPI{
+class QiitaAPI {
+  constructor() {
+    this.headers = {
+      "Authorization": "Bearer " + accessToken,
+      "Content-Type": "application/json"
+    }
+  }
 
-static post(body,title,options){
-let tags = []
-options.tags.forEach(function(name){tags.push({name: name})})
+
+  post(body, title, options) {
+    let tags = []
+    options.tags.forEach(function (name) { tags.push({ name: name }) })
     var args = {
-        data: 
-        {
-          "body": body,
-          "coediting": false,
-          "gist": false,
-          "group_url_name": "dev",
-          "private": options.private,
-          "tags": tags,
-          "title": title,
-          "tweet": false
-        },
-        headers: {"Authorization":"Bearer "+ accessToken, "Content-Type": "application/json" }
+      data:
+      {
+        "body": body,
+        "coediting": false,
+        "gist": false,
+        "group_url_name": "dev",
+        "private": options.private,
+        "tags": tags,
+        "title": title,
+        "tweet": false
+      },
+      headers: this.headers
     }
     console.log(args)
-//    args.data.tags.push
- var req = client.post(baseurl + 'items',args,function (data, response)
-  { 
-    if(response.statusCode == '201'){
-       vscode.window.showInformationMessage('hooray! successfully uploaded')
-       let article = vscode.window.activeTextEditor
-      
-     let template = {"id":data.id,"private":data.private,"tags":[]}
-       article.edit((builder)=>{
-      // let startPos = article.document.positionAt(0)
-       builder.replace(new vscode.Range(0,0,1,0),JSON.stringify(template))
-    })
-    }
-    else if(response.statusCode >= '400'){
-       vscode.window.showInformationMessage('failed to upload with error ' + response.statusCode)
-    }
-  })
-  console.log("logging reponse ")
-  console.log(req)
-}
+    //    args.data.tags.push
+    var req = client.post(baseurl + 'items', args, function (data, response) {
+      if (response.statusCode == '201') {
+        vscode.window.showInformationMessage('hooray! successfully uploaded')
+        let article = vscode.window.activeTextEditor
 
- static patch(body,title,options){
-  let tags = []
-  options.tags.forEach(function(name){tags.push({name: name})})
-  var args = {
-    data: 
-    {
-      "body": body,
-      "coediting": false,
-      "gist": false,
-      "group_url_name": "dev",
-      "private": options.private,
-      "tags":  tags,
-      "title": title,
-      "tweet": false
-    },
-    headers: {"Authorization":"Bearer "+ accessToken, "Content-Type": "application/json" }
- }
- 
- client.patch(baseurl + 'items/'+ options.id,args,function (data,response){
-    if(response.statusCode == '201'){
-      vscode.window.showInformationMessage('hooray! successfully uploaded')
+        let template = { "id": data.id, "private": data.private, "tags": options.tags }
+        article.edit((builder) => {
+          // let startPos = article.document.positionAt(0)
+          builder.replace(new vscode.Range(0, 0, 1, 0), JSON.stringify(template))
+        })
+      }
+      else if (response.statusCode >= '400') {
+        vscode.window.showInformationMessage('failed to upload with error ' + response.statusCode)
+      }
+    })
+    console.log("logging reponse ")
+    console.log(req)
+  }
+
+  patch(body, title, options) {
+    let tags = []
+    options.tags.forEach(function (name) { tags.push({ name: name }) })
+    var args = {
+      data:
+      {
+        "body": body,
+        "coediting": false,
+        "gist": false,
+        "group_url_name": "dev",
+        "private": options.private,
+        "tags": tags,
+        "title": title,
+        "tweet": false
+      },
+      headers: this.headers
     }
-    else if(response.statusCode >= '400'){
-      vscode.window.showInformationMessage('failed to upload with error ' + response.statusCode)
-    }
-  })
- }
+
+    client.patch(baseurl + 'items/' + options.id, args, function (data, response) {
+      if (response.statusCode == '201') {
+        vscode.window.showInformationMessage('hooray! successfully uploaded')
+      }
+      else if (response.statusCode >= '400') {
+        vscode.window.showInformationMessage('failed to upload with error ' + response.statusCode)
+      }
+    })
+  }
 }
 
 
